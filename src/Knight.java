5496cb55ab4a2ea8fa38283798681afcd16d7b73
KaterinaEf <katerinaeftychiou@gmail.com>
2023-07-11 15:28:33 +0300

    учебный проект

---
+++ src/Knight.java
@@ -0,0 +1,45 @@
+public class Knight {
+    private String name = "Sir Thanks-A-Lot";
+    private String weapon = "Long Sword";
+    private Boolean isGoingToSavePrincess = true;
+
+
+    public void goAndSaveThePrincess() {
+        sharpenBlade();
+        getFood();
+        assembleTeam();
+        Horse();
+        System.out.println("Да иду уже...");
+    }
+
+    private void sharpenBlade() {
+        System.out.println("Точим меч");
+    }
+
+    private void getFood() {
+        System.out.println("Собираем консервы");
+    }
+
+    private void assembleTeam() {
+        System.out.println("Будим оруженосца");
+    }
+
+    private void Horse() {
+        System.out.println("Седлаем коня");
+    }
+
+}
+
+
+
+
+
+
+    /*public void goAndSaveThePrincess() {
+        System.out.println("Точим мечь");
+        System.out.println("Собираем консервы");
+        System.out.println("Будим оруженосца");
+        System.out.println("Да иду уже...");*/
+
+
+
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        Knight knight = new Knight();
+        knight.goAndSaveThePrincess();
+
+    }
+}
